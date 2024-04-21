586bd11fb81c1d8962382bda627037df00caac59
Inventor <dreamer_777@mail.ru>
2024-04-21 21:57:34 +0300

    Разбивка по блокам, верстка модалок

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const swiper = new Swiper(".swiper", {
+// #region инициализация свайперов с брэйкпоинтами
+const brandsSwiper = new Swiper(".swiper-brands", {
     slidesPerView: "auto",
     spaceBetween: 16,
     pagination: {
@@ -13,21 +14,73 @@ const swiper = new Swiper(".swiper", {
     }
 });
 
-let hiddenEls768px = document.querySelectorAll('.hidden-768px');
-let hiddenEls1120px = document.querySelectorAll('.hidden-1120px'); 
+const devicesSwiper = new Swiper(".swiper-devices", {
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        540: {
+            enabled: false,
+            spaceBetween:0
+        }
+    }
+});
 
-const showMoreBtn = document.querySelector('button.brands__show-more');
-showMoreBtn.onclick = () => {
+const pricesSwiper = new Swiper(".swiper-services", {
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        560: {
+            enabled: false,
+            spaceBetween:0
+        }
+    }
+});
+// #endregion
+
+const btnBurger = document.querySelector('.round-button--burger');
+const btnCloseAsideMenu = document.querySelector('.btn-close');
+const asideMenu = document.querySelector('.aside-menu');
+const bodyContainer = document.querySelector('.container');
+const body = document.querySelector('.body');
+
+let showMoreBtns = document.querySelectorAll('.show-more-button');
+showMoreBtns.forEach(btn => btn.onclick = () => {
+    
+    btn.classList.toggle('show-more-button--rotated');
 
     const currentViewportWidth = window.visualViewport.width;
-
-    showMoreBtn.classList.toggle('brands__show-more--rotated');
-
-    hiddenEls1120px.forEach(el => el.classList.toggle('hidden-1120px'));
+    const currentSection = btn.parentElement.parentElement; 
+    const hiddenEls768px = currentSection.querySelectorAll(".hidden-768px");
+    const hiddenEls1120px = currentSection.querySelectorAll(".hidden-1120px");
+    // console.log(hiddenEls1120px, hiddenEls768px);
+    hiddenEls768px.forEach(el => 
+    {
+        el.classList.toggle("hidden-768px");
+    });
 
     if (currentViewportWidth < 1120) {
-        hiddenEls768px.forEach(el => el.classList.toggle('hidden-768px'));
+
+        hiddenEls1120px.forEach(el => {
+            el.classList.toggle("hidden-1120px");
+        });
     }
 
-    showMoreBtn.textContent = hiddenEls1120px[0].checkVisibility() ? "Скрыть" : "Показать все";
+    btn.textContent = hiddenEls1120px[0]?.checkVisibility() ? "Скрыть" : "Показать все";
+})
+
+btnBurger.onclick = toggleAsideMenu;
+btnCloseAsideMenu.onclick = toggleAsideMenu;
+
+function toggleAsideMenu () {
+    asideMenu.classList.toggle('hidden');
+    bodyContainer.classList.toggle('container--transparent');
+    body.classList.toggle('body--scroll-disabled');
 }
